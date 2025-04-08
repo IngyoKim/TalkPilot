d778085231a3f4fe811f76206aab663a32f7e753
Asdfuxk <asdfxxk777@gmail.com>
2025-04-08 16:33:00 +0900

    chore: 로그인 실패시 작동 구현

--- app/lib/src/pages/login_page.dart
+++ app/lib/src/pages/login_page.dart
@@ -17,6 +17,21 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+ Future<void> tryLogin({
+    required BuildContext context,
+    required Future<void> Function() loginAction,
+    required void Function(bool) setLoading,
+  }) async {
+    setLoading(true);
+    try {
+      await loginAction();
+    } catch (error) {
+      debugPrint("로그인에 실패했습니다: $error");
+    } finally {
+      setLoading(false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

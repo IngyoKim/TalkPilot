4abc698be049e83b50285245d119e81fd15d2885
Sangmin <jeonsm0404@gmail.com>
2025-04-09 16:05:13 +0900

    feat: kakao sdk 초기화 및 .env 로드 추가

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
 import 'package:talk_pilot/app.dart';
 import 'package:talk_pilot/firebase_options.dart';
@@ -9,7 +11,14 @@ Future<void> main() async {
   /// Flutter core engine 초기화
   WidgetsFlutterBinding.ensureInitialized();
 
+  /// .env 파일 로드
+  await dotenv.load(fileName: ".env");
+
+  /// Firebase 초기화
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  /// Kakao SDK 초기화
+  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']);
 
   runApp(const App());
 }

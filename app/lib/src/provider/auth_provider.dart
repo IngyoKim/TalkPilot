fb049ec9aff1b9dac8167240be60ab11f8edbf8f
Sangmin <jeonsm0404@gmail.com>
2025-04-11 21:24:37 +0900

    feat: auth_provider 구현
    - firebase_auth에서 가져온 정보를 provier를 이용하여 전역적으로 관리

---
+++ app/lib/src/provider/auth_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthProvider with ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? _user;
+
+  AuthProvider() {
+    _auth.authStateChanges().listen((user) {
+      _user = user;
+      notifyListeners();
+    });
+  }
+
+  User? get user => _user;
+  bool get isLoggedIn => _user != null;
+}

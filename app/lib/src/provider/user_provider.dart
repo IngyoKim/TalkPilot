cb5dffc7f3f78b1a2b86c0ce15d98a8c30f1256b
Sangmin <jeonsm0404@gmail.com>
2025-04-13 00:50:00 +0900

    feat: user_provider 구현
    - 앱 내부에서 user_provider를 통해서 유저 관련 정보 관리
    - login_provider에서는 최소한의 유저 관련 정보만 처리

---
+++ app/lib/src/provider/user_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'package:talk_pilot/src/models/user_model.dart';
+import 'package:talk_pilot/src/services/database/user_service.dart';
+
+class UserProvider with ChangeNotifier {
+  final UserService _userService = UserService();
+  UserModel? _currentUser;
+
+  UserModel? get currentUser => _currentUser;
+  bool get hasUser => _currentUser != null;
+
+  Future<void> loadUser(String uid) async {
+    final user = await _userService.getUser(uid);
+    if (user != null) {
+      _currentUser = user;
+    } else {
+      // 최초 로그인 시 기본 UserModel 생성
+      _currentUser = UserModel(
+        uid: uid,
+        name: '',
+        email: '',
+        nickname: '',
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      );
+      await _userService.saveUser(_currentUser!);
+    }
+    notifyListeners();
+  }
+
+  Future<void> updateUser(UserModel updatedUser) async {
+    _currentUser = updatedUser;
+    await _userService.updateUser(updatedUser);
+    notifyListeners();
+  }
+
+  void clearUser() {
+    _currentUser = null;
+    notifyListeners();
+  }
+}

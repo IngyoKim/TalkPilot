dca9b3a038df275d67e1ec8071fd65e2d338c4be
Sangmin <jeonsm0404@gmail.com>
2025-04-12 17:37:42 +0900

    feat: user_model과 user_mapper 합침
    - 굳이 책임을 분리할 정도로 규모가 크지 않음
    - 오히려 mapper를 따로 나누는게 로직이 더 복잡해짐
    
    *추가로 loginProvider, updateAt 등과 같은 요소 추가

--- app/lib/src/models/user_model.dart
+++ app/lib/src/models/user_model.dart
@@ -3,11 +3,51 @@ class UserModel {
   final String name;
   final String email;
   final String nickname;
+  final String? photoUrl;
+  final String? loginProvider;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final List<String>? projectIds;
 
   UserModel({
     required this.uid,
     required this.name,
     required this.email,
     required this.nickname,
+    this.photoUrl,
+    this.loginProvider,
+    this.createdAt,
+    this.updatedAt,
+    this.projectIds,
   });
+
+  factory UserModel.fromMap(String uid, Map<String, dynamic> map) {
+    return UserModel(
+      uid: uid,
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      nickname: map['nickname'] ?? '',
+      photoUrl: map['photoUrl'],
+      loginProvider: map['loginProvider'],
+      createdAt:
+          map['createdAt'] != null ? DateTime.tryParse(map['createdAt']) : null,
+      updatedAt:
+          map['updatedAt'] != null ? DateTime.tryParse(map['updatedAt']) : null,
+      projectIds:
+          map['projectIds'] != null ? List<String>.from(map['projectIds']) : [],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "name": name,
+      "email": email,
+      "nickname": nickname,
+      if (photoUrl != null) "photoUrl": photoUrl,
+      if (loginProvider != null) "loginProvider": loginProvider,
+      if (createdAt != null) "createdAt": createdAt!.toIso8601String(),
+      if (updatedAt != null) "updatedAt": updatedAt!.toIso8601String(),
+      if (projectIds != null) "projectIds": projectIds,
+    };
+  }
 }

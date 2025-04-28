6a1b204a087e2473cdc6f3662693bc69861abac6
Sangmin <jeonsm0404@gmail.com>
2025-04-28 12:22:05 +0900

    feat: html, css 템플릿 생성
    - 기본 틀만 잡음

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import AuthGate from './pages/AuthGate';
+// import LoginPage from './pages/LoginPage';
+// import AuthGate from './pages/AuthGate';
 
 function Router() {
     return (
         <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/*" element={<AuthGate />} />
+            {/* <Route path="/login" element={<LoginPage />} />
+            <Route path="/*" element={<AuthGate />} /> */}
         </Routes>
     );
 }

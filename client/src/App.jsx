58f91bccece6022d906f70e2c2728a25a80bdbfc
Sangmin <jeonsm0404@gmail.com>
2025-04-28 12:13:51 +0900

    feat: Router.jsx 구현
    - 페이지 분기 담당
    - 아직은 크게 기능을 하진 않음

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { UserProvider } from './contexts/UserContext';
+import { ProjectProvider } from './contexts/ProjectContext';
 import Router from './Router';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <AuthProvider>
+      <UserProvider>
+        <ProjectProvider>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </ProjectProvider>
+      </UserProvider>
+    </AuthProvider>
   );
 }
 

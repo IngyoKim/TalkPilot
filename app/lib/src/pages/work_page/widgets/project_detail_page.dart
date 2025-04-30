b14d8cca15c545476662503ff2e6a53ec4847406
Asdfuxk <asdfxxk777@gmail.com>
2025-04-30 22:35:45 +0900

    feat: 프로젝트 내 입력시 수정일 갱신

--- app/lib/src/pages/work_page/widgets/project_detail_page.dart
+++ app/lib/src/pages/work_page/widgets/project_detail_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:talk_pilot/src/models/project_model.dart';
 
+import 'package:talk_pilot/src/models/project_model.dart';
+import 'package:talk_pilot/src/provider/user_provider.dart';
+import 'package:talk_pilot/src/services/database/project_service.dart';
+
+// ignore_for_file: use_build_context_synchronously
 class ProjectDetailPage extends StatefulWidget {
   final ProjectModel project;
 
@@ -17,26 +22,57 @@ class _ProjectDetailPageState extends State<ProjectDetailPage> {
 
   String get _memoKey => 'memo_${widget.project.title}';
 
+  String _originalText = '';
+  bool _hasEdited = false;
+  bool _isReady = false;
+
   @override
   void initState() {
     super.initState();
     _memoController = TextEditingController();
     _loadSavedMemo();
-    _memoController.addListener(() {
-      _prefs.setString(_memoKey, _memoController.text);
+
+    _memoController.addListener(() async {
+      if (!_isReady) return;
+
+      final currentText = _memoController.text;
+      final edited = currentText != _originalText;
+
+      if (_hasEdited != edited) {
+        setState(() {
+          _hasEdited = edited;
+        });
+      }
+
+      _prefs.setString(_memoKey, currentText);
+
+      final uid =
+          Provider.of<UserProvider>(context, listen: false).currentUser?.uid;
+      if (uid == null) return;
+
+      final projectService = ProjectService();
+      await projectService.updateProject(widget.project.id, {
+        'updatedAt': DateTime.now().toIso8601String(),
+      });
     });
   }
 
   Future<void> _loadSavedMemo() async {
     _prefs = await SharedPreferences.getInstance();
     final savedText = _prefs.getString(_memoKey) ?? '';
+    _originalText = savedText;
     setState(() {
       _memoController.text = savedText;
     });
+    _isReady = true;
   }
 
   @override
   void dispose() {
+    if (_hasEdited) {
+      _prefs.setString(_memoKey, _memoController.text);
+    }
+
     _memoController.dispose();
     super.dispose();
   }

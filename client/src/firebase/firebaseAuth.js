cba17270b981f08bd87a2b2420dfbc8f522341da
Sangmin <jeonsm0404@gmail.com>
2025-05-01 14:52:11 +0900

    feat: .env 파일을 이용하여 firebaseConfig 구성
    추가해야할 항목들
    - client/.env에 추가하면 됨
    - FIREBASE_API_KEY
    - FIREBASE_AUTH_DOMAIN
    - FIREBASE_PROJECT_ID
    - FIREBASE_APP_ID

--- client/src/firebase/firebaseAuth.js
+++ client/src/firebase/firebaseAuth.js
@@ -2,9 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+    appId: import.meta.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);

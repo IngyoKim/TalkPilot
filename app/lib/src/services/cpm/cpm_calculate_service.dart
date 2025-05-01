0e9366388f9db410f127390faeceea265a991c27
IngyoKim <a58276976@gmail.com>
2025-05-01 16:54:53 +0900

    feat: 측정된 cpm값 DB에 업데이트
    - cpm을 측정하면 자동으로 DB의 user에 반영함

--- app/lib/src/services/cpm/cpm_calculate_service.dart
+++ app/lib/src/services/cpm/cpm_calculate_service.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:talk_pilot/src/services/database/database_service.dart';
 
 class CpmCalculateService extends ChangeNotifier {
   final String sentence = '본인의 발표 속도대로 문장을 읽어주세요.';
@@ -6,23 +8,41 @@ class CpmCalculateService extends ChangeNotifier {
   double? _cpmResult;
   bool _isRunning = false;
 
+  final _db = DatabaseService();
+  final _auth = FirebaseAuth.instance;
+
   String get displayedSentence => sentence;
   double? get cpmResult => _cpmResult;
   bool get isRunning => _isRunning;
 
-  void toggleTimer() {
+  void toggleTimer() async {
     if (!_isRunning) {
       _startTime = DateTime.now();
       _cpmResult = null;
       _isRunning = true;
     } else {
       final endTime = DateTime.now();
-      final seconds =
-          endTime.difference(_startTime!).inMilliseconds / 1000.0;
+      final seconds = endTime.difference(_startTime!).inMilliseconds / 1000.0;
       final int charCount = sentence.length;
       _cpmResult = (charCount / seconds) * 60;
       _isRunning = false;
+
+      notifyListeners();
+
+      if (_cpmResult != null) {
+        final user = _auth.currentUser;
+        if (user != null) {
+          final uid = user.uid;
+
+          /// 유저 문서에 cpm 값만 업데이트
+          await _db.updateDB("users/$uid", {
+            'cpm': _cpmResult,
+            'updatedAt': DateTime.now().toIso8601String(),
+          });
+        }
+      }
     }
+
     notifyListeners();
   }
 }

787317cab784c3a627fccd765a36718c78d751ea
Sangmin <jeonsm0404@gmail.com>
2025-05-16 18:11:32 +0900

    feat: App.jsx에서 Nest 서버 자동 로그인 구현
    - App.jsx에서 firebase 자동 로그인 로그 출력 추가
    - App.jsx에서 Nest 서버 인증 코드 추가
    
    *정상적으로 서버와 연동되어 인증이 완료됨

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
-// src/App.jsx
+import Router from './Router';
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './Contexts/AuthContext';
 import { UserProvider } from './Contexts/UserContext';
 import { ProjectProvider } from './Contexts/ProjectContext';
-import Router from './Router';
+
+import { serverLogin } from "./utils/auth/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  useEffect(() => {
+    const unsub = onAuthStateChanged(getAuth(), async (user) => {
+      if (user) {
+        console.log("[Firebase] 로그인 세션 복원됨 (자동 로그인)");
+        try {
+          const idToken = await user.getIdToken();
+          await serverLogin(idToken);
+        } catch (error) {
+          console.error("[Nest] 자동 로그인 후 서버 인증 실패", error);
+        }
+      }
+    });
+
+    return () => unsub();
+  }, []);
+
   return (
     <BrowserRouter>
       <AuthProvider>

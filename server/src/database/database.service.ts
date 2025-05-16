7a4b88f99ed623bb40832a4960d7cacd69715d8d
Sangmin <jeonsm0404@gmail.com>
2025-05-16 19:07:54 +0900

    feat: database.service.ts에 crud 구현

--- server/src/database/database.service.ts
+++ server/src/database/database.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { admin } from '../auth/firebase-admin';
 
 @Injectable()
-export class DatabaseService { }
+export class DatabaseService {
+    private db = admin.database();
+
+    async read(path: string): Promise<any> {
+        const snapshot = await this.db.ref(path).once('value');
+        return snapshot.val();
+    }
+
+    async write(path: string, data: any): Promise<void> {
+        await this.db.ref(path).set(data);
+    }
+
+    async update(path: string, data: any): Promise<void> {
+        await this.db.ref(path).update(data);
+    }
+
+    async delete(path: string): Promise<void> {
+        await this.db.ref(path).remove();
+    }
+}

4b30c104be069dc5691a901b1d3d6830f763748c
Sangmin <jeonsm0404@gmail.com>
2025-05-24 15:13:24 +0900

    feat: cpm_record_model 정의

---
+++ app/lib/src/models/cpm_record_model.dart
@@ -0,0 +1,16 @@
+class CpmRecordModel {
+  final double cpm;
+  final DateTime timestamp;
+
+  CpmRecordModel({required this.cpm, required this.timestamp});
+
+  factory CpmRecordModel.fromMap(Map<String, dynamic> map) => CpmRecordModel(
+    cpm: (map['cpm'] as num).toDouble(),
+    timestamp: DateTime.parse(map['timestamp']),
+  );
+
+  Map<String, dynamic> toMap() => {
+    'cpm': cpm,
+    'timestamp': timestamp.toIso8601String(),
+  };
+}
